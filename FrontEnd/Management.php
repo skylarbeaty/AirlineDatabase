12fe5fb833df90330e7f5340b86238ed8813f463
ssarowar <ssarowar@users.noreply.github.com>
2020-04-27 23:20:09 -0400

    Update Management.php

--- FrontEnd/Management.php
+++ FrontEnd/Management.php
@@ -41,30 +41,54 @@
                     <input type="text" name="destination_city"><br/>
                 <label>Departure City</label>
                     <input type="text" name="departure_city"><br/>
-                <label>Departure Date</label> 
-                    <input type="date" name="departure_date"><br/>
                 <label>Departure Time</label> 
                     <input type="time" name="departure_time"><br/>
                 <label>Arrival Time</label> 
                     <input type="time" name="arrival_time"><br/>
-                <label>Boarding Time</label> 
-                    <input type="time" name="boarding_time"><br/>
+                <label>Flight Length</label> 
+                    <input type="number" name="flight_length"><br/>
+                <!--<label>Boarding Time</label> 
+                    <input type="time" name="boarding_time"><br/>-->
                 <label>Total Passengers</label> 
                     <input type="number" name="total_passenger"><br/>
-                <label>Total Seats</label> 
-                    <input type="number" name="total_seats"><br/>
+                <label>Empty Seats</label> 
+                    <input type="number" name="empty_seats"><br/>
+                <label>Flight Type</label> 
+                    <input type="text" name="flight_type"><br/>
                 <label>Distance</label> 
-                    <input type="number" name="distance"><br/><br/>
+                    <input type="number" name="distance"><br/>   
+                <label>Departure Date</label> 
+                    <input type="date" name="departure_date"><br/>
+                  <label>Arrival Date</label> 
+                    <input type="date" name="arrival_date"><br/><br/>  
                 <input type="submit">
             </form>
         </div>
         <div class="col-lg-3">
             <h2 style="padding-top: 50px; padding-bottom: 20px;">Remove A Flight</h2>
             <form action="" method="POST">
-                <label>Flight Number</label> <input type="text" name="flight_no"><br/><br/>
+                <label>Flight Number</label> <input type="text" name="flight_no"><br/>
                 <input type="submit">
             </form>
+            <?php
+                include_once 'connection.php';
+
+                $flight_no = $_POST['flight_no'];
+
+
+                $sql = "DELETE FROM flight WHERE flight_no = '$flight_no'";
+
+                if ($conn->query($sql) === TRUE) {
+                echo "Flight removed successfully!";
+                }
+                else {
+                echo "Error removing flight. " . $conn->error;
+                }   
+
+                $conn->close(); 
+            ?>
         </div>
         <div class="col-lg-3 col-sm-0"></div>
+
 </body>
 </html>
