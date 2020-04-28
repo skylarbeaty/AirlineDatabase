c2ce286619174113ad549224b1ed9f4ecef890bc
ssarowar <ssarowar@users.noreply.github.com>
2020-04-28 00:20:50 -0400

    Update Insert_flight.php

--- Database/Insert_flight.php
+++ Database/Insert_flight.php
@@ -1,8 +1,9 @@
 <?php
 include_once 'connection.php';
 
-$flight_no = $_POST['flight_no'];
-$destination_city = $_POST['destination_city'];
+if (isset($_POST['submit'])){
+$flight_no = $_POST['flight_no']
+$destination_city = isset($_POST['destination_city']);
 $departure_city = $_POST['departure_city'];
 $departure_time = $_POST['departure_time'];
 $arrival_time = $_POST['arrival_time'];
@@ -16,16 +17,16 @@ $distance = $_POST['distance'];
 $departure_date = $_POST['departure_date'];
 $arrival_date = $_POST['arrival_date'];
 
-//if (isset($flight_no)) {
-	//echo "This info is rachet!";
-}
+
 $sql = "INSERT INTO flight VALUES ('$flight_no', '$destination_city', '$departure_city', '$departure_time ','$arrival_time', '$flight_length ', '$total_passengers', '$total_seats', '$empty_seats', '$flight_type', '$distance', '$departure_date ', '$arrival_date' )";
+}
 
 if ($conn->query($sql) === TRUE) {
     echo "New flight inserted successfully";
 } else {
     echo "Error: couldn't insert flight " . $sql . "<br>" . $conn->error;
 }
+
 
 $conn->close();
 ?>
